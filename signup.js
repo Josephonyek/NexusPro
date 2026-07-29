00acaa1caac87d8b4cd903a6ccf89aef47e7f5b8
Josephonyek <141643981+Josephonyek@users.noreply.github.com>
2026-07-29 14:35:12 +0100

    Update signup.js

--- signup.js
+++ signup.js
@@ -57,7 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.setItem('nexusAuthToken', result.token);
             localStorage.setItem('nexusUserId', result.userId);
             localStorage.setItem('nexusUserRole', result.role);
-
+localStorage.setItem("nx_role", userRole.toLowerCase());
+localStorage.setItem("nx_name", userName);
             btn.textContent = "Redirecting to workspace...";
             window.location.replace('dashboard.html');
 
